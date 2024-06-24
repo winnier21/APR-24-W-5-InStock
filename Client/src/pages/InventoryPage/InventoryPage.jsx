6dcf8eb939601bb9922c949313fced6f5ce3acaf
genevaguin <genevaguin@gmail.com>
2024-06-24 17:34:26 -0600

    inventorypage and styles rendered

---
+++ Client/src/pages/InventoryPage/InventoryPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemDetails from '../../components/ItemDetails/ItemDetails';
+import './InventoryPage.scss';
+
+function InventoryPage() {
+  return (
+    <div>
+      <h1>Inventory Page</h1>
+      <ItemDetails />
+    </div>
+  );
+}
+
+export default InventoryPage;
