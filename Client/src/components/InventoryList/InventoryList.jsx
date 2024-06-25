f6bde558e4410f31f30c97dc05080161ec1e649f
silvhua <silvhua@gmail.com>
2024-06-24 22:36:42 -0700

    Retrieve Inventory data from server
    - Successfully render `/inventory` URL.
    - Update imports for styling

--- Client/src/components/InventoryList/InventoryList.jsx
+++ Client/src/components/InventoryList/InventoryList.jsx
@@ -1,12 +1,26 @@
 import { useEffect, useState } from 'react';
-import InventoryList from "../InventoryCard/InventoryList";
+import InventoryCard from '../InventoryCard/InventoryCard';
+import React from 'react';
+import apiInstance from '../../utils/ApiClient';
 
 
 const InventoryList = () => {
   const [inventoryArray, setInventoryArray] = useState([]);
+
+  const getAllInventory = async () => {
+    const data = await apiInstance.getItemsArray();
+    if (data) {
+      setInventoryArray(data);
+    }
+  }
+
+  useEffect(() => {
+    getAllInventory();
+  }, [])
+
   return (
     <section>
-
+      <p>hello</p>
     </section>
   )
 }
--- Client/src/pages/InventoryPage/InventoryPage.jsx
+++ Client/src/pages/InventoryPage/InventoryPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ItemDetails from '../../components/ItemDetails/ItemDetails';
 import './InventoryPage.scss';
+import InventoryList from '../../components/InventoryList/InventoryList';
 
 function InventoryPage() {
   return (
     <div>
-      <h1>Inventory Page</h1>
+      <h1>Inventory</h1>
+      <InventoryList />
       <ItemDetails />
     </div>
   );
