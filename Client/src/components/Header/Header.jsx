7a263785ec4710d58ef87bf2e84ff2b4ae265e7b
silvhua <silvhua@gmail.com>
2024-06-25 14:00:35 -0700

    Revert "Merge pull request #17 from winnier21/feature-header/footer"
    Commit includes node_modules
    
    This reverts commit 040759163d8ea3f57bad5cfc33a543d445334308, reversing
    changes made to 011c1171e4ca7b60d585f403dc818168d363099b.

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import './Header.scss';
-import logo from '../../assets/logo/InStock-Logo_2x.png';
-import { Link } from "react-router-dom";
+import logo from '../../assets/Logo/InStock-Logo_1x.png';
+// import {link} from 'react-router-dom';
 
 function Header() {
   return (
     <nav className='header'>
-        <div className='header__container'>
-        <div className="header__logo-container"> 
-        <Link to ="/">
+        {/* <link to ="/"> */}
         <img src={logo} alt="InStock Logo" className="header__logo"/>
-        </Link>
-        </div>
-        <section className='header__wrapper'>
-        <Link className='header__link' to ='/warehouses'>
+        <section className='header__container'>
         <h3>Warehouse</h3>
-        </Link>
-        <Link className='header__link' to ='/inventory'>
         <h3>Inventory</h3>
-        </Link>
         </section>
-        </div>
+        {/* </link> */}
         </nav>
   )
 }
