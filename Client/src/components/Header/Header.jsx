c1ca2539f6e0f1925f6638eb49936e92d54acd48
winnier21 <you@example.com>
2024-06-24 20:30:07 -0700

    updated: header content

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Header.scss';
+import logo from '../../assets/Logo/InStock-Logo_1x.png';
+// import {link} from 'react-router-dom';
 
 function Header() {
   return (
-    <nav>Header</nav>
+    <nav className='header'>
+        {/* <link to ="/"> */}
+        <img src={logo} alt="InStock Logo" className="header__logo"/>
+        <section className='header__container'>
+        <h3>Warehouse</h3>
+        <h3>Inventory</h3>
+        </section>
+        {/* </link> */}
+        </nav>
   )
 }
 
