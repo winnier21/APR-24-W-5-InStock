72ae9dc4dc6b133363b78f65634d1f56afee6aba
winnier21 <you@example.com>
2024-06-25 14:50:56 -0700

    added some variables

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WarehousePage from './pages/WarehousePage/WarehousePage';
 import InventoryPage from './pages/InventoryPage/InventoryPage';
-import './App.scss'
+import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<WarehousePage />} />
         <Route path="/inventory" element={<InventoryPage />} />
