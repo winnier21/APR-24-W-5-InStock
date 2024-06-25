58c9a9942a05ff144c3b7ee844a5316725c05806
winnier21 <you@example.com>
2024-06-25 11:49:25 -0700

    header styling

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import WarehousePage from './pages/WarehousePage/WarehousePage';
-import InventoryPage from './pages/InventoryPage/InventoryPage';
-import './App.scss'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import Header from "./components/Header/Header";
+import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<WarehousePage />} />
         <Route path="/inventory" element={<InventoryPage />} />
