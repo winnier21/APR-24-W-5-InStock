579fdb9fa580900d5185fcb045e501cb8a5af5d7
silvhua <silvhua@gmail.com>
2024-06-26 10:21:07 -0700

    Merge branch 'develop' into inventory-list

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
-import WarehousePage from './pages/WarehousePage/WarehousePage';
-import InventoryPage from './pages/InventoryPage/InventoryPage';
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import ItemDetailsPage from './pages/ItemDetailsPage/ItemDetailsPage';
 import EditWarehousePage from './pages/EditWarehousePage/EditWarehousePage';
 import EditItemPage from './pages/EditItemPage/EditItemPage';
 import AddWarehousePage from './pages/AddWarehousePage/AddWarehousePage';
 import AddItemPage from './pages/AddItemPage/AddItemPage';
-import './App.scss'
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<WarehousePage />} />
       <Route path="/warehouse" element={<WarehousePage />} />
@@ -20,10 +23,11 @@ function App() {
       <Route path="/warehouse/:warehouseId/edit" element={<EditWarehousePage />} />
       <Route path="/warehouse/add" element={<AddWarehousePage />} />
       <Route path="/inventory" element={<InventoryPage />} />
-      <Route path="/inventory/:itemId" element={<ItemDetailsPage />} />
+      {/* <Route path="/inventory/:itemId" element={<ItemDetailsPage />} />
       <Route path="/inventory/:itemId/edit" element={<EditItemPage />} />
-      <Route path="/inventory/add" element={<AddItemPage />} />
+      <Route path="/inventory/add" element={<AddItemPage />} /> */}
     </Routes>
+    <Footer />
   </BrowserRouter>
   );
 }
