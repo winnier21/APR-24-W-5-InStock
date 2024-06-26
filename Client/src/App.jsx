b8797ceb610862eb46c3d141bc40a4e64b192c15
winnier21 <you@example.com>
2024-06-25 17:09:45 -0700

    editing main

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import WarehousePage from './pages/WarehousePage/WarehousePage';
-import InventoryPage from './pages/InventoryPage/InventoryPage';
-import './App.scss';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    
-    <Header />
-    <main>
-      <Routes>
-        <Route path="/" element={<WarehousePage />} />
-        <Route path="/inventory" element={<InventoryPage />} />
-      </Routes>
-      </main>
-    <Footer />
+      <Header />
+     
+        <Routes>
+          <Route path="/" element={<WarehousePage />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+        </Routes>
+     
+      <Footer />
     </BrowserRouter>
   );
 }
