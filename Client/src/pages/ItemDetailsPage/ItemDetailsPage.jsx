0fd3ea58a8c47ab1e8529d5055bf3ab8cd79d01c
silvhua <silvhua@gmail.com>
2024-07-01 00:51:37 -0700

    create BEM selectors

--- Client/src/pages/ItemDetailsPage/ItemDetailsPage.jsx
+++ Client/src/pages/ItemDetailsPage/ItemDetailsPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import Placeholder from '../../components/Placeholder/Placeholder';
 import apiInstance from '../../utils/ApiClient';
 import FormHeader from '../../components/FormHeader/FormHeader';
-// import './ItemDetailsPage.scss';
+import './ItemDetailsPage.scss';
 
 const ItemDetailsPage = () => {
   const itemId = useParams().itemId;
@@ -23,6 +23,9 @@ const ItemDetailsPage = () => {
   if (!itemObject) {
     return <Placeholder />
   }
+  const {
+    id, warehouse_name, item_name, description, category, status, quantity
+  } = itemObject;
 
   return (
     <main>
@@ -30,7 +33,25 @@ const ItemDetailsPage = () => {
         title="Edit"
         editPath="inventory"
       />
+      <article className="item">
+        <section className="item__section">
+          <h4 className="item__heading">
+            Item Description
+          </h4>
+          <p className="item__p">
+            {description}
+          </p>
+          <h4 className="item__heading">
+
+          </h4>
+          <p className="item__p">
+
+          </p>
+        </section>
+        
+      </article>
     </main>
+
   )
 }
 
