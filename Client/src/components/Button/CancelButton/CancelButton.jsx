8cbb743813ba5de902d31a833967575dd162c3ea
winnier21 <you@example.com>
2024-06-30 17:40:07 -0700

    cancel button fixed the onclick function

--- Client/src/components/Button/CancelButton/CancelButton.jsx
+++ Client/src/components/Button/CancelButton/CancelButton.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './CancelButton.scss';
 
-const CancelButton = ({ onClick }) => {
+const CancelButton = () => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate(-1); // This will go back to the previous page
+  };
+
   return (
-    <button className="button-cancel" type="button" onClick={onClick}>
+    <button className="button-cancel" type="button" onClick={handleCancel}>
       <h3>Cancel</h3>
     </button>
   );
