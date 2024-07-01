ea10cb1a17eb91558bb5739b0fac84573baf2fc8
genevaguin <genevaguin@gmail.com>
2024-07-01 10:53:56 -0600

    - deleted additempage scss - text area removed resizable - font styles included in each input as wasn't working collectively

--- Client/src/pages/AddItemPage/AddItemPage.jsx
+++ Client/src/pages/AddItemPage/AddItemPage.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import './AddItemPage.scss';
-// import AddItem from '../../components/AddItemForm/AddItemForm'
-// import AddItemForm2 from '../../components/AddItemForm/AddItemForm2';
-import AddItemForm from '../../components/AddItemForm/AddItemForm';
 import FormHeader from '../../components/FormHeader/FormHeader';
-import apiInstance from '../../utils/ApiClient';
 import EditItemForm from '../../components/EditItemForm/EditItemForm';
 
 
@@ -25,11 +20,7 @@ function AddItemPage({ warehousesProps }) {
       <EditItemForm
         warehousesProps={warehousesProps}
         itemObject={itemObject}
-        // requestCallback={apiInstance.}
       />
-      {/* <AddItemForm/> */}
-      {/* <AddItemForm2 warehousesProps={warehousesProps} /> */}
-      {/* <AddItem warehousesProps={warehousesProps} /> */}
     </main>
   );
 }
