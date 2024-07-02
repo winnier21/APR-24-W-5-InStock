e4563b97da386b39afa53505aa6b590bb82e04a5
silvhua <silvhua@gmail.com>
2024-07-01 22:44:38 -0700

    Add chevron icon to warehouse & inventory cards

--- Client/src/components/CardLink/CardLink.jsx
+++ Client/src/components/CardLink/CardLink.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Cardlink.scss";
+import chevron from '../../assets/icons/chevron_right-24px.svg'
 
 const Cardlink = (props) => {
   const { id, className, content, route } = props;
+
   return (
-    <Link to={`/${route}/${id}`} className={className}>
+    <Link to={`/${route}/${id}`} className={`${className} card-link__arrow`}>
       <h3 className="card-link">{content}</h3>
+      <img
+        src={chevron}
+        className="card-link__arrow" 
+        alt="forward arrow"
+      />
     </Link>
   );
 };
